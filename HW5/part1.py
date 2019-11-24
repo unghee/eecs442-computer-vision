1db44526157539f77b07c82c24829caae5283ae4
JordLee <unghee@umich.edu>
2019-11-24 13:50:14 -0500

    part2 validation set

---
+++ HW5/part1.py
@@ -0,0 +1,118 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm # Displays a progress bar
+
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from torch.utils.data import Dataset, Subset, DataLoader, random_split
+
+# Load the dataset and train, val, test splits
+print("Loading datasets...")
+FASHION_transform = transforms.Compose([
+    transforms.ToTensor(), # Transform from [0,255] uint8 to [0,1] float
+    transforms.Normalize([0.2859], [0.3530]) # Normalize to zero mean and unit variance
+])
+FASHION_trainval = datasets.FashionMNIST('.', download=True, train=True, transform=FASHION_transform)
+FASHION_train = Subset(FASHION_trainval, range(50000))
+FASHION_val = Subset(FASHION_trainval, range(50000,60000))
+FASHION_test = datasets.FashionMNIST('.', download=True, train=False, transform=FASHION_transform)
+print("Done!")
+
+# Create dataloaders
+# TODO: Experiment with different batch sizes
+trainloader = DataLoader(FASHION_train, batch_size=64, shuffle=True)
+valloader = DataLoader(FASHION_val, batch_size=64, shuffle=True)
+testloader = DataLoader(FASHION_test, batch_size=64, shuffle=True)
+
+class Network(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # TODO: Design your own network, define layers here.
+        # Here We provide a sample of two-layer fully-connected network from HW4 Part3.
+        # Your solution, however, should contain convolutional layers.
+        # Refer to PyTorch documentations of torch.nn to pick your layers. (https://pytorch.org/docs/stable/nn.html)
+        # Some common Choices are: Linear, Conv2d, ReLU, MaxPool2d, AvgPool2d, Dropout
+        # If you have many layers, consider using nn.Sequential() to simplify your code
+        # self.fc1 = nn.Linear(28*28, 8) # from 28x28 input image to hidden layer of size 256
+        # self.fc2 = nn.Linear(8,10) # from hidden layer to 10 class scores
+        self.sclayer1 = nn.Sequential(
+            nn.Conv2d(1, 12, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.sclayer2 = nn.Sequential(
+            nn.Conv2d(12, 24, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.drop_out = nn.Dropout()
+        self.fc1 = nn.Linear(7 * 7 * 24, 500)
+        self.fc2 = nn.Linear(500, 10)
+
+
+    def forward(self,x):
+        # TODO: Design your own network, implement forward pass here
+        # x = x.view(-1,28*28) # Flatten each image in the batch
+        # x = self.fc1(x)
+        # relu = nn.ReLU() # No need to define self.relu because it contains no parameters
+        # x = relu(x)
+        # x = self.fc2(x)
+        # # The loss layer will be applied outside Network class
+        # x = x.view(-1,28,28,1)
+        x = self.sclayer1(x)
+        x = self.sclayer2(x)
+        x = x.reshape(x.size(0), -1)
+        # x = x.view(-1,7 * 7 * 32)
+        x = self.drop_out(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+
+        return x
+
+device = "cuda" if torch.cuda.is_available() else "cpu" # Configure device
+print('GPU USED?',torch.cuda.is_available())
+model = Network().to(device)
+criterion = nn.CrossEntropyLoss() # Specify the loss layer
+# TODO: Modify the line below, experiment with different optimizers and parameters (such as learning rate)
+optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-4) # Specify optimizer and assign trainable parameters to it, weight_decay is L2 regularization strength
+num_epoch = 20
+
+# TODO: Choose an appropriate number of training epochs
+
+def train(model, loader, num_epoch = 20): # Train the model
+    print("Start training...")
+    model.train() # Set the model to training mode
+    for i in range(num_epoch):
+        running_loss = []
+        for batch, label in tqdm(loader):
+            batch = batch.to(device)
+            label = label.to(device)
+            optimizer.zero_grad() # Clear gradients from the previous iteration
+            pred = model(batch) # This will call Network.forward() that you implement
+            loss = criterion(pred, label) # Calculate the loss
+            running_loss.append(loss.item())
+            loss.backward() # Backprop gradients to all tensors in the network
+            optimizer.step() # Update trainable weights
+        print("Epoch {} loss:{}".format(i+1,np.mean(running_loss))) # Print the average loss for this epoch
+    print("Done!")
+
+def evaluate(model, loader): # Evaluate accuracy on validation / test set
+    model.eval() # Set the model to evaluation mode
+    correct = 0
+    with torch.no_grad(): # Do not calculate grident to speed up computation
+        for batch, label in tqdm(loader):
+            batch = batch.to(device)
+            label = label.to(device)
+            pred = model(batch)
+            correct += (torch.argmax(pred,dim=1)==label).sum().item()
+    acc = correct/len(loader.dataset)
+    print("Evaluation accuracy: {}".format(acc))
+    return acc
+    
+train(model, trainloader, num_epoch)
+print("Evaluate on validation set...")
+evaluate(model, valloader)
+print("Evaluate on test set")
+evaluate(model, testloader)
+
